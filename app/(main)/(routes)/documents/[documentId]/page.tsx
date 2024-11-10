ef38be36cfb9420418a9865f29bc35f8fcb50256
Astra321 <50966736+Astra321@users.noreply.github.com>
2024-11-10 14:20:21 +0200

    comit

---
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -0,0 +1,9 @@
+
+'use client';
+
+import { useMemo } from 'react';
+import Cover from '@/components/cover';
+import { Toolbar } from '@/components/toolbar';
+import { Skeleton } from '@/components/ui/skeleton';
+
+// The rest of the file content remains the same
---
+++ hooks/useOrigin.tsx
@@ -0,0 +1,6 @@
+
+'use client';
+
+import { useEffect } from 'react';
+
+// Hook content remains the same
