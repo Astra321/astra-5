cf1eb2be187250959a9cd992c19f390104e3d7b4
Astra321 <50966736+Astra321@users.noreply.github.com>
2024-11-10 21:08:42 +0200

    dd

--- components/cover.tsx
+++ components/cover.tsx
@@ -1,43 +1,10 @@
 
-'use client';
+import Image from 'next/image';
 
-import { useEffect, useState } from 'react';
-import { getStorage, ref, getDownloadURL } from 'firebase/storage';
-import { initializeApp } from 'firebase/app';
-
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const storage = getStorage(app);
-
-export default function Cover() {
-  const [coverUrl, setCoverUrl] = useState('');
-
-  useEffect(() => {
-    const fetchCoverUrl = async () => {
-      try {
-        const coverRef = ref(storage, 'covers/coverImage.jpg');
-        const url = await getDownloadURL(coverRef);
-        setCoverUrl(url);
-      } catch (error) {
-        console.error('Error fetching cover image URL:', error);
-      }
-    };
-
-    fetchCoverUrl();
-  }, []);
-
-  return (
+const Cover = ({ src, alt }) => (
     <div>
-      {coverUrl && <img src={coverUrl} alt="Cover Image" />}
+        <Image src={src} alt={alt} layout="responsive" width={500} height={300} />
     </div>
-  );
-}
+);
+
+export default Cover;
--- components/spinner.tsx
+++ components/spinner.tsx
@@ -1,14 +1,1 @@
-'use client';
-
-import { Loader } from 'lucide-react';
-import React from 'react';
-
-const Spinner = () => {
-  return (
-    <div className="spinner">
-      <Loader />
-    </div>
-  );
-};
-
-export default Spinner;
+export default function Spinner() { return <div>Loading...</div>; }
